7dda6da3e579ff6d0f68ffa05664c752af0318eb
jeremybenaim <jeremybenaim@gmail.com>
2014-08-29 15:17:55 -0400

    moved youtube-dl dependency to npm package

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express'),
     router = express.Router();
 
 var request = require('request'),
+    youtubedl = require('youtube-dl'),
     fs = require('fs'),
     child_process = require('child_process');
 
@@ -17,19 +18,23 @@ router.get('/yt/:youtube_video_id.mp3', function(req, res) {
   /*res.setHeader("Cache-Control", "public, max-age=2592000");
   res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());*/
   
-  child_process.exec("youtube-dl --simulate -j http://www.youtube.com/watch?v=" + req.params.youtube_video_id, function(err, stdout, stderr) {
-    var youtube_dl_JSON = JSON.parse(stdout.toString());
+  var url = 'http://www.youtube.com/watch?v=' + req.params.youtube_video_id;
+
+  youtubedl.getInfo(url, [], function(err, data) {
+    if (err) {
+      return res.send(500);
+    }
 
     res.setHeader('Accept-Ranges','bytes');
     res.setHeader('Content-Type', 'audio/mp3');
 
     var ffmpeg_child = child_process.spawn("ffmpeg", ['-i', 'pipe:0', '-acodec', 'libmp3lame', '-f', 'mp3', '-']);
 
-    request.get(youtube_dl_JSON.url, {headers: {'Youtubedl-no-compression': 'True'}}).pipe(ffmpeg_child.stdin);
+    request.get(data.url, {headers: {'Youtubedl-no-compression': 'True'}}).pipe(ffmpeg_child.stdin);
 
     ffmpeg_child.stdout.pipe(res);
-
   });
+
 });
 
 module.exports = router;
