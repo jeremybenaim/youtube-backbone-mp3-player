92a106cd215c08c53b05c3560ba5ab45ba0d94bf
jeremybenaim <jeremybenaim@gmail.com>
2014-08-29 16:40:43 -0400

    changed a couple of stuff, added the 'audio ended' event thingie

--- TODOS.md
+++ TODOS.md
@@ -1,7 +1,6 @@
 ## Todos
 
 - error handling (for failing spawned process among others)
-- audios end event (fucking up the updateTimer function)
 - get duration (in tracks.json?) to increment the elapsedTime div width value on updateTimer
 - cross-fade between songs (create new Audio then smoothPause audio A when audio B canplay event is fired)
 - add other streaming sources supported by youtube-dl
--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,7 @@
     escape: /\<\@\-(.+?)\@\>/gim
   };
 
+  /* A track */
   JB.TrackModel = Backbone.Model.extend({
     defaults : {
       'id': '',
@@ -18,6 +19,7 @@
     }
   });
 
+  /* A track in the list */
   JB.SingleTrackView = Backbone.Marionette.ItemView.extend({
     tagName: 'li',
     template: '#trackTemplate',
@@ -34,15 +36,18 @@
     }
   });
 
+  /* The list */
   JB.PlayList = Backbone.Collection.extend({
     model: JB.TrackModel
   });
 
+  /* The list (just a <ul> tag really) */
   JB.PlayListView = Backbone.Marionette.CollectionView.extend({
     tagName: 'ul',
     childView: JB.SingleTrackView
   });
 
+  /* The player model, sort of.. */
   JB.PlayerModel = Backbone.Model.extend({
     defaults : {
       'id': '',
@@ -51,6 +56,7 @@
     }
   });
 
+  /* The spaghetti plate */
   JB.Player = Backbone.Marionette.CompositeView.extend({
     template: '#playerTemplate',
     model: new JB.PlayerModel(),
@@ -59,17 +65,15 @@
     },
     initialize: function () {
       var self = this;
-      
-      /* Player is paused by default */
-      this.$el.addClass('paused');
 
       /* Bind Events */
       JB.on('track:click', function(model){
         if (self.model.id === model.id) {
           self.togglePlayPause();
         } else { 
-          self.$el.find('h1 .timer').text('');
+          $('.player').removeClass('playing');
           self.$el.find('h1 .title').text('Loading...');
+          self.$el.find('h1 .timer').text('');
           self.playTrack(model);
         }
       });
@@ -83,6 +87,13 @@
         console.log('♫ start playing track: "'+ data.title+ '" ♫');
       });
     },
+    initPlayer: function () {
+      var self = this;
+      this.model.clear().set(self.model.defaults);
+      self.$el.find('h1 .title').text('nothing to play :(');
+      self.$el.find('h1 .timer').text('');
+      $('.player').removeClass('playing');
+    },
     togglePlayPause: function(){
       var audio = $('audio')[0] || null;
 
@@ -93,7 +104,7 @@
       } else {
         window.utils.audio.smoothPause(audio, 200);
       }
-      $('.player').toggleClass('paused playing');
+      $('.player').toggleClass('playing');
     },
     playTrack: function (model) {
       var self = this;
@@ -112,7 +123,7 @@
       
       var play = function () {
         self.$el.find('h1 .title').text(model.title);
-        self.$el.addClass('playing').removeClass('paused');
+        self.$el.addClass('playing');
         JB.trigger('player:play', model);
       };
       var updateTimer = function () {
@@ -123,9 +134,13 @@
         
         self.$el.find('h1 .timer').text('[' + timer + ']');
       };
+      var reinit = function () {
+        self.initPlayer();
+      };
 
       $audio.on('canplay', play);
       $audio.on('timeupdate', updateTimer);
+      $audio.on('ended', reinit);
     },
     findOrCreateAudioElement: function () {
       var audioElm = this.$el.find('audio');
@@ -147,6 +162,10 @@
   });
 
 
+  /* 
+   * booting all the thing 
+   */
+  
   /* Create a new TrackList */
   var tracklist = new JB.PlayList(window.tracks),
       playlist = new JB.PlayListView({ /* and associate it to a view */
@@ -169,116 +188,5 @@
     e.preventDefault();
     JB.trigger('keyboard:spacebar');
   }, true);
+
 })();
-
-
-window.utils = window.utils || {};
-window.utils.audio = {};
-
-/**
-* Start audio playing with fade in period.
-*
-* @param {Object} audio HTML5 audio element
-* @param {Number} (optional) rampTime How long is the fade in ms
-* @param {Number} targetVolume Max volume. 1 = default = HTML5 audio max.
-* @param {Number} tick Timer period in ms
-*/
-window.utils.audio.smoothPlay = function(audio, rampTime, targetVolume, tick) {
-  if(!targetVolume) {
-     targetVolume = 1;
-  }
-
-  // By default, ramp up in one second
-  if(!rampTime) {
-     rampTime = 1000;
-  }
-
-  // How often adjust audio volume (ms)
-  if(!tick) {
-    tick = 10;
-  }
-
-  var volumeIncrease = targetVolume / (rampTime / tick);
-
-  var playingEventHandler = null;
-
-  function ramp() {
-    var vol = Math.min(targetVolume, audio.volume + volumeIncrease);
-
-    audio.volume = vol;
-
-    // Have we reached target volume level yet?
-    if(audio.volume < targetVolume) {
-      // Keep up going until 11
-      setTimeout(ramp, tick);
-    }
-  }
-
-  function startRampUp() {
-    // For now, we capture only the first playing event
-    // as we assume the user calls fadeIn()
-    // every time when wants to resume playback
-    audio.removeEventListener("playing", playingEventHandler);
-
-    ramp();
-  }
-
-  // Start with zero audio level
-  audio.volume = 0;
-
-  // Start volume ramp up when the audio actually stars to play (not when begins to buffer, etc.)
-  audio.addEventListener("playing", startRampUp);
-
-  audio.play();
-};
-
-/**
-* Stop audio playing with fade out period.
-*
-* @param {Object} audio HTML5 audio element
-* @param {Number} (optional) rampTime How long is the fade in ms
-* @param {Number} targetVolume Min volume. 0 = default = HTML5 audio min.
-* @param {Number} tick Timer period in ms
-*/
-window.utils.audio.smoothPause = function(audio, rampTime, targetVolume, tick) {
-  var orignalVolume = audio.volume;
-
-  if(!targetVolume) {
-    targetVolume = 0;
-  }
-
-  // By default, ramp up in one second
-  if(!rampTime) {
-    rampTime = 1000;
-  }
-
-  // How often adjust audio volume (ms)
-  if(!tick) {
-    tick = 10;
-  }
-
-  var volumeStep = (audio.volume - targetVolume) / (rampTime / tick);
-
-  if(!volumeStep) {
-    // Volume already at 0
-    return;
-  }
-
-  function ramp() {
-    var vol = Math.max(0, audio.volume - volumeStep);
-
-    audio.volume = vol;
-    // Have we reached target volume level yet?
-    if(audio.volume > targetVolume) {
-      // Keep up going until 11
-      setTimeout(ramp, tick);
-    } else {
-      audio.pause();
-      // Reset audio volume so audio can be played again
-      audio.volume = orignalVolume;
-    }
-  }
-
-  ramp();
-};
-
